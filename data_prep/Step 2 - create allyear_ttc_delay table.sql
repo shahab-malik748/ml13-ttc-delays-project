fd6988cc4759f6d82cc88c1791f6f345ab95cd50
Arman <aakavalekar@gmail.com>
2025-11-10 17:52:01 -0500

    Submitting data prep work Includes final data frame we will use to model.

---
+++ data_prep/Step 2 - create allyear_ttc_delay table.sql
@@ -0,0 +1,77 @@
+/*
+DROP TABLE IF EXISTS allyears_ttc_delays;
+
+CREATE TABLE allyears_ttc_delays AS
+  SELECT * FROM ttc_subway_delay_jan_2014_april_2017
+  UNION ALL SELECT * FROM ttc_subway_delay_may_december_2017
+  UNION ALL SELECT * FROM March_2018
+  UNION ALL SELECT * FROM April_2018
+  UNION ALL SELECT * FROM May_2018
+  UNION ALL SELECT * FROM June_2018
+  UNION ALL SELECT * FROM July_18
+  UNION ALL SELECT * FROM August_18
+  UNION ALL SELECT * FROM Sept_18
+  UNION ALL SELECT * FROM Oct_18
+  UNION ALL SELECT * FROM Nov_2018
+  UNION ALL SELECT * FROM Dec_2018
+  UNION ALL SELECT * FROM Jan_18
+  UNION ALL SELECT * FROM Feb_18
+  UNION ALL SELECT * FROM Mar_19
+  UNION ALL SELECT * FROM Apr_19
+  UNION ALL SELECT * FROM May_19
+  UNION ALL SELECT * FROM June_19
+  UNION ALL SELECT * FROM July_19
+  UNION ALL SELECT * FROM August_19
+  UNION ALL SELECT * FROM September_19
+  UNION ALL SELECT * FROM Oct_19
+  UNION ALL SELECT * FROM Nov_19
+  UNION ALL SELECT * FROM December_19
+  UNION ALL SELECT * FROM Jan_19
+  UNION ALL SELECT * FROM Feb_19
+  UNION ALL SELECT * FROM Jan_2020
+  UNION ALL SELECT * FROM Feb_20
+  UNION ALL SELECT * FROM March_20
+  UNION ALL SELECT * FROM May_20
+  UNION ALL SELECT * FROM June_20
+  UNION ALL SELECT * FROM July_20
+  UNION ALL SELECT * FROM August_20
+  UNION ALL SELECT * FROM Sept_20
+  UNION ALL SELECT * FROM Oct_20
+  UNION ALL SELECT * FROM Nov_20
+  UNION ALL SELECT * FROM Dec_2020
+  UNION ALL SELECT * FROM January_21
+  UNION ALL SELECT * FROM Feb_21
+  UNION ALL SELECT * FROM March_21
+  UNION ALL SELECT * FROM April_21
+  UNION ALL SELECT * FROM May_21
+  UNION ALL SELECT * FROM June_21
+  UNION ALL SELECT * FROM July_21
+  UNION ALL SELECT * FROM August_21
+  UNION ALL SELECT * FROM Sept_21
+  UNION ALL SELECT * FROM Oct_21
+  UNION ALL SELECT * FROM Nov_21
+  UNION ALL SELECT * FROM December_21
+  UNION ALL SELECT * FROM ttc_subway_delay_data_2022
+  UNION ALL SELECT * FROM ttc_subway_delay_data_2023
+  UNION ALL SELECT * FROM ttc_subway_delay_data_2024
+  UNION ALL SELECT * FROM TTC_Subway_Delay_Data_since_2025
+;
+*/
+
+--Some checks
+
+select count (*) from allyears_ttc_delays;
+-- 228,870
+select count (*) from allyears_ttc_delays where "Min Delay" = 0;
+-- 149,074
+select count (*) from allyears_ttc_delays where "Min Delay" > 0 
+-- 79,796
+
+
+--If we want to look at only delays > 10 mins:
+select count (*) from allyears_ttc_delays where "Min Delay" > 10 and line <> 'SHP';
+-- 10,325
+
+select distinct line from allyears_ttc_delays;
+
+select distinct day from allyears_ttc_delays;
---
+++ data_prep/Step 3 - create final ttc delay view.sql
@@ -0,0 +1,79 @@
+/*
+DROP TABLE IF EXISTS ttc_delays_final_df;
+
+create table ttc_delays_final_df as
+select
+a.Date as date
+,cast(strftime('%Y', a.Date) as integer) as year
+,cast(strftime('%m', a.Date) as integer) as month
+,cast(strftime('%d', a.Date) as integer) as day_of_month
+,cast(strftime('%H', a.Time) as integer) as hour
+,cast(strftime('%M', a.Time) as integer) as minute
+,cast(strftime('%w', a.Date) as integer) as weekday_num
+,a.Time as time
+,cast(strftime('%H', a.Time) as integer) as hour
+,cast(strftime('%M', a.Time) as integer) as minute
+,a.Day as day
+,cast(strftime('%w', a.Date) as INTEGER) as day_of_week
+,a.Station as station
+,a.Code as delay_code
+,b."CODE DESCRIPTION" as delay_code_desc
+,b."Code classification by cause" as delay_category
+,a."Min Delay" as min_delay
+,a.Bound as location_bound
+,case
+    when a.Line in ('YUS', 'YU/BD', 'YU') then 'YU'
+    when a.Line in ('BD', 'BD Line 2') then 'BD'
+    else a.Line
+  end as line
+,case
+	when a."Min Delay" >= 10 then 1
+	else 0
+	end as major_delay_flag
+,case
+    when a.Day in ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday') and
+         (strftime('%H:%M', a.Time) between '06:30' and '09:30' or
+         strftime('%H:%M', a.Time) between '16:00' and '19:00') then 1
+    else 0
+	end as rush_hour_flag
+,b."Controllable Flag" as controllable_delay_flag
+,case
+	when strftime('%m-%d', a.Date) between '03-20' and '06-20' then 'Spring'
+	when strftime('%m-%d', a.Date) between'06-21' and '09-21' then 'Summer'
+	when strftime('%m-%d', a.Date) between '09-22' and '12-20' then 'Fall'
+	else 'Winter'
+    end as season
+,case
+	when c.Date is not null then 1
+	else 0
+	end as major_event_flag
+,c."Event Description" as major_event_desc
+from allyears_ttc_delays a
+inner join ttc_delay_codes_classified_with_cnc_flag b on b."SUB RMENU CODE" = a.Code -- with this join we are removing scarborough line rows
+left join major_events c on c."Date" = a."Date"
+where a."Min Delay" > 0 -- remove rows with no delay
+and a.Line not in ('20 CLIFFSIDE', '109 RANEE', '77 SWANSEA', '25 DON MILLS'); -- remove bus stop rows
+
+
+*/
+
+-- Checks 
+SELECT name FROM sqlite_master WHERE type='table' AND name='ttc_delays_final_df';
+
+select count (*) from allyears_ttc_delays where "Min Delay" > 0;
+-- 79796
+select count (*) from ttc_delays_final_df where min_delay > 0;
+-- 75596
+
+
+select count (*) from ttc_delays_final_df where rush_hour_flag = 1 
+-- 20,595 rush hour delays (6:30-9:30 and 16:00 to 19:00 on weekdays)
+
+select count (*) from ttc_delays_final_df where major_delay_flag = 1
+-- 11,660 major delays (>=10 mins delay)
+
+select count (*) from ttc_delays_final_df where rush_hour_flag = 1 and major_delay_flag = 1
+-- 2,472 major delays during rush hour
+
+
+*/
